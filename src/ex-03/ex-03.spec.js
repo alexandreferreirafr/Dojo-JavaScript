4592f872577dbc0a87c42ae8cc9fef6d60e0d327
Alexandre FERREIRA <alexandre.ferreira@axa.fr>
2019-04-15 17:28:39 +0200

    fix ex-03 spec

--- src/ex-03/ex-03.spec.js
+++ src/ex-03/ex-03.spec.js
@@ -1,12 +1,95 @@
 import { factorial } from './ex-03';
 
-test("La fonction renvoie le bonne factorielle de 1 à 10", () => {
-    const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const outputs = [1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800]
+test("La factorielle de 1 est égal à 1", () => {
+    const inputs = 1
+    const outputs = 1
 
-    inputs.forEach(async (item, index) => {
-        const expected = await factorial(item)
+    const expected = factorial(input)
 
-        expect(expected).toEqual(outputs[index])
-    })
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 2 est égal à 2", () => {
+    const inputs = 2
+    const outputs = 2
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 3 est égal à 6", () => {
+    const inputs = 3
+    const outputs = 6
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 4 est égal à 24", () => {
+    const inputs = 4
+    const outputs = 24
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 5 est égal à 120", () => {
+    const inputs = 5
+    const outputs = 120
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 6 est égal à 720", () => {
+    const inputs = 6
+    const outputs = 720
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 7 est égal à 5040", () => {
+    const inputs = 7
+    const outputs = 5040
+
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 8 est égal à 40320", () => {
+    const inputs = 8
+    const outputs = 40320
+
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 9 est égal à 362880", () => {
+    const inputs = 9
+    const outputs = 362880
+
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
+})
+
+test("La factorielle de 10 est égal à 3628800", () => {
+    const inputs = 10
+    const outputs = 3628800
+
+
+    const expected = factorial(input)
+
+    expect(expected).toEqual(output)
 })
